0d74a6c25826cdfeaf4f797257f4d32d4107a45f
Barbara Makarova <makarena2001barbara@yandex.ru>
2023-03-08 16:30:26 +0300

    first

---
+++ src/test/java/ru/netology/web/CardTest.java
@@ -0,0 +1,48 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CardTest {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestV1() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Макарова Варвара");
+        driver.findElement(By.cssSelector("input[type='tel']")).sendKeys("+79856543211");
+        driver.findElement(By.tagName("label")).click();
+        driver.findElement(By.className("button__text")).click();
+
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.tagName("p")).getText();
+
+        assertEquals(expected, actual);
+    }
+}
